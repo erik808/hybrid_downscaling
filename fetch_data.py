130790734f26d4374b35feca07a4c661196d2f38
erik <erik.mulder@smhi.se>
2024-06-10 15:43:30 +0200

    quite happy with xesmf

--- fetch_data.py
+++ fetch_data.py
@@ -12,7 +12,6 @@ box_Channel['max_lat'] = 50.36
 box_Channel['min_dep'] = 0.4940253794193268
 box_Channel['max_dep'] = 155.85072326660156
 
-
 # box for the Norwegian coastal current test area
 box_NwCC = {}
 box_NwCC['min_lon'] = 4.2
@@ -22,90 +21,39 @@ box_NwCC['max_lat'] = 58.7
 box_NwCC['min_dep'] = 0.4940253794193268
 box_NwCC['max_dep'] = 643.5668334960938
 
-dataset_id = "cmems_mod_nws_phy_anfc_0.027deg-2D_PT15M-i"
-dataset_id = "cmems_mod_nws_phy-uv_my_7km-2D_PT1H-i"
+dataset_id_HR_static = "cmems_mod_nws_phy_anfc_0.027deg-3D_static"
+dataset_part="bathy"
+
+dataset_id_HR = "cmems_mod_nws_phy_anfc_0.027deg-2D_PT15M-i"
+dataset_id_LR = "cmems_mod_nws_phy-uv_my_7km-2D_PT1H-i"
+
 variables = ["uo", "vo"]
 time_start = "2023-01-01T23:00:00"
 time_end = "2023-02-01T23:00:00"
 
-out = cm.subset(
-    dataset_id=dataset_id,
-    variables=variables,
-    minimum_longitude=box_NwCC['min_lon'],
-    maximum_longitude=box_NwCC['max_lon'],
-    minimum_latitude=box_NwCC['min_lat'],
-    maximum_latitude=box_NwCC['max_lat'],
-    start_datetime=time_start,
-    end_datetime=time_end,
-    minimum_depth=box_NwCC['min_dep'],
-    maximum_depth=box_NwCC['max_dep'],
-    force_download=False,
-    netcdf_compression_enabled=True,
-    # output_filename="data.nc",
-    overwrite_output_data=True,
-    overwrite_metadata_cache=False,
-    netcdf_compression_level=0,
-)
+variables=["deptho", "deptho_lev", "mask"]
 
-path = str(out)
-ds=xr.open_dataset(path)
-Nlat = len(ds.latitude)
-Nlon = len(ds.longitude)
-Ntime = len(ds.time)
-Npoints = Nlat*Nlon
-u = np.reshape(ds.uo.values, (Ntime, Npoints)).T
-v = np.reshape(ds.vo.values, (Ntime, Npoints)).T
+out = fetch_wrapper(box_NwCC,
+                    dataset_id=dataset_id_HR_static,
+                    dataset_part="bathy",
+                    variables=variables,
+                    start_datetime=time_start,
+                    end_datetime=time_end)
 
-reorder = np.vstack([np.arange(Npoints),
-                     np.arange(Npoints, 2*Npoints)]).reshape((-1,),
-                                                             order='F')
-
-
-X = np.vstack([u,v])
-X = X[reorder,:]
-
-
-Xkp1 = X[:,2:]
-Xk = X[:,1:-1]
-Xkm1 = X[:,:-2]
-
-# secant predictor
-Uk = 2*Xk - Xkm1
-
-u_pr,v = get_uv(Uk)
-u_tr,v = get_uv(Xkp1)
-diff = np.abs(u_tr-u_pr)
-
-
-plt.close('all');
-plt.subplot(2,2,1)
-plt.imshow(u_pr[100,:,:]);
-plt.gca().invert_yaxis();
-plt.subplot(2,2,2)
-plt.imshow(u_tr[100,:,:]);
-plt.gca().invert_yaxis();
-plt.subplot(2,2,3)
-plt.imshow(diff[100,:,:]);
-plt.gca().invert_yaxis();
-plt.pause(1);
-
-
-def get_uv(X):
-    Nt = X.shape[1]
-    u = np.reshape((X[0::2, :]).T, (Nt, Nlat, Nlon))
-    v = np.reshape((X[1::2, :]).T, (Nt, Nlat, Nlon))    
-    return u,v
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def fetch_wrapper(box, **kwargs):
+    out = cm.subset(
+        minimum_longitude=box['min_lon'],
+        maximum_longitude=box['max_lon'],
+        minimum_latitude=box['min_lat'],
+        maximum_latitude=box['max_lat'],
+        minimum_depth=box['min_dep'],
+        maximum_depth=box['max_dep'],
+        force_download=True,
+        netcdf_compression_enabled=True,
+        # output_filename="data.nc",
+        overwrite_output_data=True,
+        overwrite_metadata_cache=False,
+        netcdf_compression_level=0,
+        **kwargs
+    )
+    return out
--- tmp2.py
+++ tmp2.py
@@ -2,16 +2,23 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 import xesmf as xe
+import time
 
 HR_data_file = ('cmems_mod_nws_phy_anfc_0.027deg-2D_PT15M-i_'
                 'uo-vo_4.23E-7.78E_56.81N-58.70N_2023-01-01-2023-02-01.nc')
+
+HR_bathy_file = ('cmems_mod_nws_phy_anfc_0.027deg-3D_'
+                 'static_multi-vars_4.23E-7.78E_56.81N-58.70N_0.49-643.57m.nc')
+
 LR_data_file = ('cmems_mod_nws_phy-uv_my_7km-2D_PT1H-i_'
                 'uo-vo_4.22E-7.78E_56.80N-58.67N_2023-01-01-2023-02-01.nc')
+
+bt_HR = xr.open_dataset(HR_bathy_file)
 
 ds_HR = xr.open_dataset(HR_data_file)
 ds_LR = xr.open_dataset(LR_data_file)
 
-def build_grid(ds):
+def build_grid(ds, mask=None):
     lat_arr = ds.latitude
     lon_arr = ds.longitude
     Nlat = lat_arr.shape[0]
@@ -21,44 +28,49 @@ def build_grid(ds):
     grid = {}
     grid['N'] = Nlat
     grid['M'] = Nlon
-    grid['lat'] = lat_grid
-    grid['lon'] = lon_grid
+    grid['lat'] = np.ascontiguousarray(lat_grid)
+    grid['lon'] = np.ascontiguousarray(lon_grid)
+    if mask is not None:
+        grid['mask'] = mask
     return grid
 
-grid_HR = build_grid(ds_HR)
+grid_HR = build_grid(ds_HR, bt_HR.mask[0,:,:])
 grid_LR = build_grid(ds_LR)
 
-plt.scatter(grid_HR['lon'], grid_HR['lat'], s=0.01)
+tic = time.time()
+interp_HR_LR = xe.Regridder(grid_HR, grid_LR, "bilinear", extrap_method="inverse_dist")
+interp_LR_HR = xe.Regridder(grid_LR, grid_HR, "bilinear", extrap_method="inverse_dist")
 
-tstep=400
-ufield_HR = ds_HR.uo[tstep,:,:]
+ufield_HR = ds_HR.uo.rename({'longitude':'lon',
+                             'latitude':'lat'})
 
-interp_u_HR = LinearNDInterpolator( (grid_HR['lon'].flatten(),
-                                     grid_HR['lat'].flatten() ), ufield_HR.values.flatten())
+u_HR_LR = interp_HR_LR(ufield_HR.values)
+u_HR_LR_HR = interp_LR_HR(u_HR_LR)
 
-u_HR_LR = interp_u_HR( grid_LR['lon'].flatten(),
-                       grid_LR['lat'].flatten() )
+da_HR_LR = xr.DataArray(u_HR_LR, dims=['time','lat','lon'],
+                        coords={'time':ds_HR.time,
+                                'lat':ds_LR.latitude.values,
+                                'lon':ds_LR.longitude.values})
 
-u = xr.zeros_like(ds_LR.uo)
-u[tstep,:,:] = np.reshape(u_HR_LR, (grid_LR['N'], grid_LR['M']))
+da_HR_LR_HR = xr.zeros_like(ufield_HR)
+da_HR_LR_HR[:,:,:] = u_HR_LR_HR
 
+for tstep in range(0,len(ufield_HR.time),10):
 
-plt.close('all');
-plt.subplot(2,1,1)
-u[tstep,:,:].plot.imshow()
-plt.subplot(2,1,2)
-ds_LR.uo[tstep+1,:,:].plot.imshow()
-plt.pause(1)
+    plt.clf()
+    plt.subplot(2,2,1)
+    ufield_HR[tstep,:,:].plot.imshow()
+    plt.gca().set_title('')
+    plt.subplot(2,2,2)
+    da_HR_LR[tstep,:,:].plot.imshow()
+    plt.gca().set_title('')
+    plt.subplot(2,2,3)
+    da_HR_LR_HR[tstep,:,:].plot.imshow()
+    plt.gca().set_title('')
+    plt.subplot(2,2,4)
+    (da_HR_LR_HR-ufield_HR)[tstep,:,:].plot.imshow()
+    plt.gca().set_title('')
+    plt.pause(.01)
 
-
-
-
-
-
-
-
-
-
-
-
-
+toc = time.time()
+print(f'{toc-tic}')

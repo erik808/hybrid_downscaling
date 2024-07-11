5ecc2f5fad3cb97bb0523f50dfec69e84743ee14
erik <erik.mulder@smhi.se>
2024-07-11 17:00:03 +0200

    working keras based autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -1,24 +1,130 @@
 from importlib import reload
 
-import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
-import xesmf as xe
-import os
-import time
-from multiprocess import Pool
-from dask.diagnostics import ProgressBar
+
 from datetime import datetime
+
+
 from sklearn.preprocessing import MinMaxScaler
 
+import keras
+from keras import layers
+from keras import ops
+from keras.models import Model
 
 import data_manager as dm
-reload(dm)
 
-scaler = MinMaxScaler(feature_range=(0,1))
-data_HR = scaler.fit_transform(dm.da_HR.values.reshape(dm.Nt, -1))\
-                .reshape(dm.Nt, dm.Nlat, dm.Nlon)
-data_LR = scaler.transform(dm.da_LR.values.reshape(dm.Nt, -1))\
-                .reshape(dm.Nt, dm.Nlat, dm.Nlon)
+Nt, Nlat, Nlon = dm.da_HR.shape
 
+scaled_range = (0,1)
 
+scaler = MinMaxScaler(feature_range=scaled_range)
+data = scaler.fit_transform(dm.da_HR.values.reshape(Nt, -1))\
+             .reshape(Nt, Nlat, Nlon,1)
+
+# plt.close('all')
+# plt.imshow(data[0,:,:]);
+# plt.gca().invert_yaxis();
+# plt.pause(1)
+
+Nt, Nlat, Nlon, Nchannels = data.shape
+split = int(Nt*4/5)
+train_range = range(0, split)
+test_range = range(split, Nt)
+
+train_data = data[train_range,:,:]
+test_data = data[test_range,:,:]
+
+## Build an autoencoder with Keras using the functional API
+keras.utils.clear_session(free_memory=True)
+create_model=True
+model_path_autoencoder = 'models/autoencoder.keras'
+model_path_encoder = 'models/encoder.keras'
+model_path_decoder = 'models/decoder.keras'
+if create_model:
+
+    state_input = layers.Input(shape=(Nlat, Nlon, 1),
+                               name="full_state_input")
+    # Encoding layers
+    e1 = layers.Conv2D(32, (3,3), activation="relu",
+                       padding="same")(state_input)
+    e2 = layers.MaxPooling2D((2,2),
+                             padding="same")(e1)
+    e3 = layers.Conv2D(32, (3,3), activation="relu",
+                       padding="same")(e2)
+    encoded = layers.MaxPooling2D((2,2),
+                                  padding="same")(e3)
+
+    encoder = Model(state_input, encoded, name="encoder")
+    encoder.summary(60)
+
+    # Decoder
+    d1 = layers.Conv2DTranspose(32, (3,3), strides=2, activation="relu",
+                                padding="same")(encoded)
+    d2 = layers.Conv2DTranspose(32, (3,3), strides=2, activation="relu",
+                                padding="same")(d1)
+    d3 = layers.Conv2D(1, (3,3), activation="sigmoid",
+                       padding="same")(d2)
+    decoded = layers.Cropping2D(cropping=((2,1),(2,1)))(d3)
+
+    decoder = Model(encoded, decoded, name="decoder")
+    decoder.summary(60)
+
+    autoencoder = Model(state_input, decoded, name="autoencoder")
+    autoencoder.summary(60)
+
+elif isinstance(model_path, str):
+    encoder = keras.saving.load_model(model_path_encoder)
+    decoder = keras.saving.load_model(model_path_decoder)
+    autoencoder = keras.saving.load_model(model_path_autoencoder)
+else:
+    raise Exception('nope')
+
+loss = keras.losses.MeanSquaredError(
+        reduction="sum_over_batch_size",
+        name="mean_squared_error"
+    )
+autoencoder.compile(optimizer='adam',
+                    loss=loss)
+
+epochs = 10
+batch_size = 50
+shuffle = True
+autoencoder.fit(
+    x=train_data,
+    y=train_data,
+    epochs=epochs,
+    batch_size=batch_size,
+    shuffle=shuffle,
+    validation_data=(test_data, test_data)
+    )
+
+# save models
+autoencoder.save(model_path)
+encoder.save(model_path)
+decoder.save(model_path)
+
+predictions = autoencoder.predict(test_data)
+encoded_data = encoder.predict(test_data)
+decoded_data = decoder.predict(encoded_data)
+
+id = 100
+plt.close()
+plt.subplot(2,2,1)
+plt.imshow(test_data[id,:,:,0])
+plt.gca().invert_yaxis()
+
+plt.subplot(2,2,2)
+plt.imshow(predictions[id,:,:,0])
+plt.gca().invert_yaxis()
+
+plt.subplot(2,2,3)
+plt.imshow(encoded_data[id,:,:,0])
+plt.gca().invert_yaxis()
+
+plt.subplot(2,2,4)
+plt.imshow(decoded_data[id,:,:,0])
+plt.gca().invert_yaxis()
+plt.tight_layout()
+plt.pause(1)
--- data_manager.py
+++ data_manager.py
@@ -11,10 +11,6 @@ HR_bathy_file = ('cmems_mod_nws_phy_anfc_0.027deg-3D_'
 LR_data_file = ('cmems_mod_nws_phy-uv_my_7km-2D_PT1H-i_'
                 'uo-vo_4.22E-7.78E_56.80N-58.67N_2023-01-01-2023-05-01.nc')
 
-bt_HR = xr.open_dataset(HR_bathy_file)
-ds_HR = xr.open_dataset(HR_data_file)
-ds_LR = xr.open_dataset(LR_data_file)
-
 def build_grid(ds, mask=None):
     lat_arr = ds.latitude
     lon_arr = ds.longitude
@@ -31,30 +27,37 @@ def build_grid(ds, mask=None):
         grid['mask'] = mask
     return grid
 
-grid_HR = build_grid(ds_HR, bt_HR.mask[0,:,:])
-grid_LR = build_grid(ds_LR)
+def load_data():
+    bt_HR = xr.open_dataset(HR_bathy_file)
+    ds_HR = xr.open_dataset(HR_data_file)
+    ds_LR = xr.open_dataset(LR_data_file)
 
-interp_HR_LR = xe.Regridder(grid_HR, grid_LR, "bilinear",
-                            extrap_method="inverse_dist")
-interp_LR_HR = xe.Regridder(grid_LR, grid_HR, "bilinear",
-                            extrap_method="inverse_dist")
+    grid_HR = build_grid(ds_HR, bt_HR.mask[0,:,:])
+    grid_LR = build_grid(ds_LR)
 
-da_HR = ds_HR.uo.rename({'longitude':'lon',
-                         'latitude':'lat'})
+    interp_HR_LR = xe.Regridder(grid_HR, grid_LR, "bilinear",
+                                extrap_method="inverse_dist")
+    interp_LR_HR = xe.Regridder(grid_LR, grid_HR, "bilinear",
+                                extrap_method="inverse_dist")
 
-da_HR = da_HR.fillna(0.0)
+    da_HR = ds_HR.uo.rename({'longitude':'lon',
+                             'latitude':'lat'})
 
-da_HR_LR = interp_HR_LR(da_HR.values)
-da_HR_LR_HR_tmp = interp_LR_HR(da_HR_LR)
+    da_HR = da_HR.fillna(0.0)
 
-da_HR_LR = xr.DataArray(da_HR_LR, dims=['time','lat','lon'],
-                        coords={'time':ds_HR.time,
-                                'lat':ds_LR.latitude.values,
-                                'lon':ds_LR.longitude.values})
+    da_HR_LR = interp_HR_LR(da_HR.values)
+    da_HR_LR_HR_tmp = interp_LR_HR(da_HR_LR)
 
-da_HR_LR_HR = xr.zeros_like(da_HR)
-da_HR_LR_HR[:,:,:] = da_HR_LR_HR_tmp
-da_LR = da_HR_LR_HR.fillna(0.0)
+    da_HR_LR = xr.DataArray(da_HR_LR, dims=['time','lat','lon'],
+                            coords={'time':ds_HR.time,
+                                    'lat':ds_LR.latitude.values,
+                                    'lon':ds_LR.longitude.values})
 
-Nt, Nlat, Nlon = da_HR.shape
-Nt, Nlat_LR, Nlon_LR = da_HR_LR.shape
+    da_HR_LR_HR = xr.zeros_like(da_HR)
+    da_HR_LR_HR[:,:,:] = da_HR_LR_HR_tmp
+    da_LR = da_HR_LR_HR.fillna(0.0)
+
+    return da_HR, da_LR
+
+
+da_HR,_ = load_data()

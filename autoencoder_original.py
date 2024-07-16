6c2161e5c7b4de6ee543dde6aad6cea800d7f48c
erik mulder <erik.mulder@smhi.se>
2024-07-16 13:43:12 +0200

    using a stride for the convolutional layer instead of maxpooling

--- autoencoder_original.py
+++ autoencoder_original.py
@@ -21,25 +21,29 @@ from keras import layers
 from keras import ops
 from keras.models import Model
 
+from multiprocess import Pool
+
 import data_manager as dm
 reload(dm)
 
-new_experiment=False
+new_experiment=True
 if new_experiment:
     create_model_from_scratch=True
     experiment_id = datetime.now().strftime('%Y%m%d_%H%M%S')
     # experiment_id = 'testing'
 else:
     create_model_from_scratch=False
-    experiment_id = '20240715_165252'
+    experiment_id = '20240716_104034'
 
 models_dir = f'experiments/{experiment_id}/models'
 results_dir = f'experiments/{experiment_id}/results'
+movie_dir = f'experiments/{experiment_id}/movies'
 checkpoints_dir = f'experiments/{experiment_id}/checkpoints'
 
 log_file = f'{models_dir}/log.txt'
 
 os.system(f'mkdir -p {models_dir}')
+os.system(f'mkdir -p {movie_dir}')
 os.system(f'mkdir -p {results_dir}')
 os.system(f'mkdir -p {checkpoints_dir}')
 
@@ -50,12 +54,9 @@ scaled_range = (0,1)
 
 # StandardScaler doesnt work that well
 scaler_HR = MinMaxScaler(feature_range=scaled_range)
-scaler_LR = MinMaxScaler(feature_range=scaled_range)
-
 data_HR = scaler_HR.fit_transform(dm.da_HR.values.reshape(Nt, -1))\
                    .reshape(Nt, Nlat, Nlon,1)
-
-data_LR = scaler_LR.fit_transform(dm.da_LR.values.reshape(Nt, -1))\
+data_LR = scaler_HR.transform(dm.da_LR.values.reshape(Nt, -1))\
                    .reshape(Nt, Nlat, Nlon,1)
 
 plt.close('all')
@@ -70,6 +71,10 @@ stacked_data = data_HR
 
 train_data = stacked_data[train_range,:,:,:]
 test_data = stacked_data[test_range,:,:,:]
+
+test_LR = data_LR[test_range,:,:,:]
+train_time = dm.da_LR.time.values[train_range]
+test_time  = dm.da_LR.time.values[test_range]
 
 # create mask to be used in network
 mask = torch.tensor(dm.mask.values)[None,:,:,None]
@@ -109,31 +114,37 @@ class Masking(layers.Layer):
 if create_model_from_scratch:
     num_filters = 32
     num_channels = train_data.shape[-1]
+    masking_layer1 = Masking(mask, name="masking_layer1")
+    masking_layer2 = Masking(mask, name="masking_layer2")
     state_input = layers.Input(shape=(Nlat, Nlon, num_channels),
                                name="full_state_input")
     # Encoding layers
-    e1 = layers.Conv2D(num_filters, (3,3), activation="relu",
+    e1 = layers.Conv2D(num_filters, (5,5), strides = (2,2),
+                       activation="relu",
                        padding="same")(state_input)
-    e2 = layers.MaxPooling2D((2,2),
-                             padding="same")(e1)
-    e3 = layers.Conv2D(num_filters, (3,3), activation="relu",
-                       padding="same")(e2)
-    encoded = layers.MaxPooling2D((2,2),
-                                  padding="same")(e3)
+    # e2 = masking_layer1(e1)
+    # e3 = layers.MaxPooling2D((2,2),
+    #                          padding="same")(e2)
+    encoded = layers.Conv2D(num_filters, (5,5), strides = (2,2),
+                       activation="relu",
+                       padding="same")(e1)
+
+    # encoded = layers.MaxPooling2D((2,2),
+    #                               padding="same")(e4)
 
     encoder = Model(state_input, encoded, name="encoder")
     encoder.summary(60)
 
     # Decoder
-    d1 = layers.Conv2DTranspose(num_filters, (3,3), strides=2, activation="relu",
+    d1 = layers.Conv2DTranspose(num_filters, (5,5), strides=(2,2), activation="relu",
                                 padding="same")(encoded)
-    d2 = layers.Conv2DTranspose(num_filters, (3,3), strides=2, activation="relu",
+    d2 = layers.Conv2DTranspose(num_filters, (5,5), strides=(2,2), activation="relu",
                                 padding="same")(d1)
     d3 = layers.Conv2D(num_channels, (3,3), activation="sigmoid",
                        padding="same")(d2)
     cropped = layers.Cropping2D(cropping=((2,1),(2,1)))(d3)
-    masking_layer = Masking(mask)
-    decoded = masking_layer(cropped)
+
+    decoded = masking_layer2(cropped)
 
     decoder = Model(encoded, decoded, name="decoder")
     decoder.summary(60)
@@ -178,7 +189,7 @@ if train_model:
     #     embeddings_metadata=None,
     # )
 
-    epochs = 100
+    epochs = 10
     batch_size = 50
     shuffle = True
     tic = time.time()
@@ -200,8 +211,9 @@ if train_model:
 
 print('create predictions')
 predictions = autoencoder.predict(test_data)
-encoded_data = encoder.predict(test_data)
-decoded_data = decoder.predict(encoded_data)
+encoded_xr_HR_true = encoder.predict(test_data)
+encoded_xr_HR_pred = encoder.predict(predictions)
+encoded_xr_LR_true = encoder.predict(test_LR)
 
 # write log
 original = sys.stdout
@@ -210,71 +222,99 @@ with open(log_file, 'w') as f:
     print(autoencoder.summary(100))
     sys.stdout = original
 
+# Create dictionary for output visualization
+xr_HR_true_fun = lambda i : scaler_HR.inverse_transform(test_data[i,:,:,0]\
+                                                   .reshape(1,-1))\
+                                .reshape(Nlat, Nlon)
 
+xr_HR_pred_fun = lambda i : scaler_HR.inverse_transform(predictions[i,:,:,0]\
+                                                        .reshape(1,-1))\
+                                     .reshape(Nlat, Nlon)
 
+xr_HR_diff_fun = lambda i : xr_HR_true_fun(i) - xr_HR_pred_fun(i)
 
-# FACTORIZE THIS:
+Rs_true_fun = lambda i : test_data[i,:,:,0] - test_LR[i,:,:,0]
+Rs_pred_fun = lambda i : predictions[i,:,:,0] - test_LR[i,:,:,0]
+Rs_diff_fun = lambda i : Rs_true_fun(i) - Rs_pred_fun(i)
+
+enc_xr_HR_true_fun = lambda i : (encoded_xr_HR_true[i,:,:,0])
+enc_xr_LR_true_fun = lambda i : (encoded_xr_LR_true[i,:,:,0])
+enc_xr_HR_pred_fun = lambda i : (encoded_xr_HR_pred[i,:,:,0])
+
+output_dict = {'xr_HR true' : {'values' : xr_HR_true_fun,
+                               'vmin' : -.8,
+                               'vmax' : .8, 'cmap' : 'RdBu'},
+               'xr_HR pred' : {'values' : xr_HR_pred_fun,
+                               'vmin' : -.8,
+                               'vmax' : .8, 'cmap' : 'RdBu'},
+               'xr_HR diff' : {'values' : xr_HR_diff_fun,
+                               'vmin' : -0.2,
+                               'vmax' : 0.2, 'cmap' : 'RdBu'},
+
+               'res true' : {'values' : Rs_true_fun,
+                             'vmin' : -0.2,
+                             'vmax' : 0.2, 'cmap' : 'RdBu'},
+               'res pred' : {'values' : Rs_pred_fun,
+                             'vmin' : -0.2,
+                             'vmax' : 0.2, 'cmap' : 'RdBu'},
+               'res dif' : {'values' : Rs_diff_fun,
+                            'vmin' : -0.1,
+                            'vmax' : 0.1, 'cmap' : 'RdBu'},
+
+               'enc xr_HR true' : {'values' : enc_xr_HR_true_fun,
+                                   'vmin' : 0,
+                                   'vmax' : .5, 'cmap' : 'viridis'},
+               'enc xr_HR pred' : {'values' : enc_xr_HR_pred_fun,
+                                   'vmin' : 0,
+                                   'vmax' : .5, 'cmap' : 'viridis'},
+               'enc xr_LR true' : {'values' : enc_xr_LR_true_fun,
+                                   'vmin' : 0,
+                                   'vmax' : .5, 'cmap' : 'viridis'},
+               }
+
+# FACTORIZE THIS further:
 # Analysis
+def plot_frame(id, fig_name=None):
+    shrinkf=0.5
+    plt.clf()
+    if fig_name == None:
+        fig_name = f'{movie_dir}/frame-{id:06d}.png'
 
-id = 100
-plt.close('all')
+    for f, (key, item) in enumerate(output_dict.items()):
+        plt.subplot(3,3,f+1)
+        h = plt.imshow(item['values'](id),
+                       cmap=item['cmap'],
+                       vmin=item['vmin'],
+                       vmax=item['vmax'])
+        plt.colorbar(h, shrink=shrinkf)
+        plt.gca().set_title(key)
+        plt.gca().invert_yaxis()
+
+    plt.suptitle(f"date: {np.datetime64(test_time[id], 'h')}")
+    print(fig_name)
+    plt.savefig(fig_name)
+
 fig = plt.figure(figsize=(15, 13))
-shrinkf=0.6
-
-plt.subplot(3,3,1)
-xr_HR_true = scaler_HR.inverse_transform(test_data[id,:,:,0]\
-                                             .reshape(1,-1))\
-                          .reshape(Nlat, Nlon)
-
-h = plt.imshow(xr_HR_true, cmap='RdBu')
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-
-plt.subplot(3,3,2)
-xr_HR_pred = scaler_HR.inverse_transform(predictions[id,:,:,0]\
-                                         .reshape(1,-1))\
-                      .reshape(Nlat, Nlon)
-h = plt.imshow(xr_HR_pred, cmap='RdBu')
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-
-plt.subplot(3,3,3)
-diff=xr_HR_true-xr_HR_pred
-h = plt.imshow(diff,cmap='RdBu')
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-plt.gca().set_title(f'{np.linalg.norm(diff.reshape(-1))}')
-plt.pause(1)
-
-plt.subplot(3,3,4)
-h = plt.imshow(test_data[id,:,:,0],)
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-
-plt.subplot(3,3,5)
-h = plt.imshow(predictions[id,:,:,0])
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-
-plt.subplot(3,3,6)
-diff = test_data[id,:,:,0]-predictions[id,:,:,0]
-h = plt.imshow(diff)
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().set_title(f'{np.linalg.norm(diff.reshape(-1))}')
-plt.gca().invert_yaxis()
-
-plt.subplot(3,3,7)
-h = plt.imshow(encoded_data[id,:,:,0],)
-plt.colorbar(h, shrink=shrinkf)
-plt.gca().invert_yaxis()
-
-
 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 fig_name = f'{results_dir}/results_autoencoder_{timestamp}.png'
-print(fig_name)
+plot_frame(100, fig_name)
 
-plt.tight_layout()
-plt.savefig(fig_name)
+fig = plt.figure(figsize=(15, 8))
+with Pool(8) as p:
+    p.map(plot_frame, range(0,len(test_time),4))
+
+movie_name = f'movie_{timestamp}.mov'
+framerate = 24
+sys_cmd = ( f"ffmpeg -r {framerate} -f image2 -pattern_type glob -i "
+            f"'{movie_dir}/frame-*.png' "
+            f"-vcodec libx264 -crf 25  -pix_fmt yuv420p -y "
+            f"{movie_dir}/{movie_name}" )
+
+print(sys_cmd)
+os.system(sys_cmd)
+sys_cmd = ( f"rm {movie_dir}/frame-*.png")
+print(sys_cmd)
+os.system(sys_cmd)
 
 fig_name = f'{results_dir}/history_{timestamp}.png'
 plt.close('all')

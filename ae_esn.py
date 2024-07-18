b884532f901ffd225db366acd1410877b76e4a41
erik mulder <erik.mulder@smhi.se>
2024-07-18 17:26:57 +0200

    began work on ae-esn integration

---
+++ ae_esn.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from datetime import datetime
+import time
+from importlib import reload
+import numpy as np
+import matplotlib.pyplot as plt
+
+import keras
+
+# base experiment
+base_exp = '20240718_164427_tf_multiply'
+models_dir = f'experiments/{base_exp}/models'
+ae_esn_dir = f'experiments/{base_exp}/ae_esn_experiments'
+os.system(f'mkdir -p {ae_esn_dir}')
+
+model_path_encoder = f'{models_dir}/encoder_res.keras'
+model_path_decoder = f'{models_dir}/decoder_res.keras'
+# load trained encoder and decoder
+encoder = keras.models.load_model(model_path_encoder)
+decoder = keras.models.load_model(model_path_decoder)
+

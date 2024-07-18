de287e3e3ed213f52d85f77c90ed6eb949ee23d6
erik mulder <erik.mulder@smhi.se>
2024-07-18 17:47:53 +0200

    need to factorize training data creation

--- ae_esn.py
+++ ae_esn.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 
 import keras
 
+import data_manager as dm
+reload(dm)
+
 # base experiment
 base_exp = '20240718_164427_tf_multiply'
 models_dir = f'experiments/{base_exp}/models'
@@ -19,4 +22,27 @@ model_path_decoder = f'{models_dir}/decoder_res.keras'
 # load trained encoder and decoder
 encoder = keras.models.load_model(model_path_encoder)
 decoder = keras.models.load_model(model_path_decoder)
+
+# load uv data
+da_HR, da_LR, da_mask = dm.load_uv_data()
+
+# FACTORIZE training data creation:
+# TODO dm.create_training_data()
+
+# do the assembling into channels here
+data_HR_stacked = np.stack([da_HR['uo'].values,
+                            da_HR['vo'].values], axis=3)
+data_LR_stacked = np.stack([da_LR['uo'].values,
+                            da_LR['vo'].values], axis=3)
+
+Nt, Nlat, Nlon, num_channels = data_HR_stacked.shape
+
+# StandardScaler doesnt work that well
+scaler_HR = MinMaxScaler(feature_range=scaled_range)
+data_HR = scaler_HR.fit_transform(data_HR_stacked.reshape(Nt, -1))\
+                   .reshape(Nt, Nlat, Nlon, num_channels)
+data_LR = scaler_HR.transform(data_LR_stacked.reshape(Nt, -1))\
+                   .reshape(Nt, Nlat, Nlon, num_channels)
+
+
 

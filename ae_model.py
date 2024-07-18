3ca5ae57b00519f3ce0f911d36c9a6bc6c5aea93
erik mulder <erik.mulder@smhi.se>
2024-07-18 10:58:02 +0200

    implementing hypermodel here as well

--- ae_model.py
+++ ae_model.py
@@ -1,4 +1,7 @@
+import sys
+
 import keras
+import keras_tuner
 from keras import layers
 from keras import ops
 from keras.models import Model
@@ -25,79 +28,142 @@ class Masking(layers.Layer):
     def call(self, inputs):
         return ops.multiply(inputs, self.mask)
 
-def build_model(conv_arch='7_conv_layers',
-                learning_rate=0.001,
-                use_dropout=True,
-                activation='relu',
-                mask=None):
-    
-    num_filters = 32
-    num_filters_red = 32
-    kernel_size = (3,3)
+class AutoEncoder(keras_tuner.HyperModel):
 
-    masking_layer1 = Masking(mask, name="masking_layer1")
-    masking_layer2 = Masking(mask, name="masking_layer2")
-    state_input = layers.Input(shape=(Nlat, Nlon, num_channels),
-                               name="full_state_input")
-    
-    # Encoder ------------------------------------------------------
-    x = layers.Conv2D(num_filters, kernel_size, strides = (2,2),
-                      activation=activation,
-                      padding="same")(state_input)
+    def __init__(self, test_vec, mask, log_file):
+        super(AutoEncoder, self).__init__()
 
-    if conv_arch == '7_conv_layers':
+        self.test_vec = test_vec
+        self.mask = mask
+        self.log_file = log_file
+
+        self.log('AutoEncoder\n', 'w')
+
+    def build_model(self,
+                    conv_arch='7_conv_layers',
+                    learning_rate=0.001,
+                    use_dropout=False,
+                    activation='relu',
+                    optimizer='adam',
+                    verbosity=0,
+                    ):
+
+        Nlat, Nlon, num_channels = self.test_vec.shape
+        num_filters = 32
+        num_filters_red = 32
+        kernel_size = (3,3)
+
+        masking_layer = Masking(self.mask, name="masking_layer")
+        state_input = layers.Input(shape=(Nlat, Nlon, num_channels),
+                                   name="full_state_input")
+
+        # Encoder ------------------------------------------------------
         x = layers.Conv2D(num_filters, kernel_size, strides = (2,2),
                           activation=activation,
-                          padding="same")(x)
+                          padding="same")(state_input)
 
-    if use_dropout:
-        x = layers.Dropout(0.25)(x)
+        if conv_arch == '7_conv_layers':
+            x = layers.Conv2D(num_filters, kernel_size, strides = (2,2),
+                              activation=activation,
+                              padding="same")(x)
 
-    encoded = layers.Conv2D(num_filters_red, kernel_size, strides = (2,2),
-                       activation=activation,
-                       padding="same")(x)
+        if use_dropout:
+            x = layers.Dropout(0.25)(x)
 
-    encoder = Model(state_input, encoded, name="encoder")
-    encoder.summary(60)
+        encoded = layers.Conv2D(num_filters_red, kernel_size, strides = (2,2),
+                           activation=activation,
+                           padding="same")(x)
 
-    # Decoder ------------------------------------------------------
-    y = layers.Conv2DTranspose(num_filters, kernel_size, strides=(2,2), activation=activation,
-                               padding="same")(encoded)
-    if conv_arch == '7_conv_layers':
-        y = layers.Conv2DTranspose(num_filters, kernel_size, strides=(2,2), activation=activation,
-                                   padding="same")(y)
+        encoder = Model(state_input, encoded, name="encoder")
+        if verbosity > 10:
+            encoder.summary(60)
 
-    if use_dropout:
-        y = layers.Dropout(0.25)(y)
+        # Decoder ------------------------------------------------------
+        y = layers.Conv2DTranspose(num_filters, kernel_size,
+                                   strides=(2,2),
+                                   activation=activation,
+                                   padding="same")(encoded)
+        if conv_arch == '7_conv_layers':
+            y = layers.Conv2DTranspose(num_filters, kernel_size,
+                                       strides=(2,2), activation=activation,
+                                       padding="same")(y)
 
-    y = layers.Conv2DTranspose(num_filters, kernel_size, strides=(2,2), activation=activation,
-                                padding="same")(y)
-    y = layers.Conv2D(num_channels, kernel_size, activation="sigmoid",
-                       padding="same")(y)
+        if use_dropout:
+            y = layers.Dropout(0.25)(y)
 
-    if conv_arch == '7_conv_layers':
-        cropped = layers.Cropping2D(cropping=((1,2),(3,4)))(y)
-        
-    elif conv_arch == '5_conv_layers':
-        cropped = layers.Cropping2D(cropping=((2,1),(2,1)))(y)
-    else:
-        raise Exception(f'invalid conv_arch {conv_arch}')
+        y = layers.Conv2DTranspose(num_filters, kernel_size,
+                                   strides=(2,2), activation=activation,
+                                    padding="same")(y)
+        y = layers.Conv2D(num_channels, kernel_size, activation="sigmoid",
+                           padding="same")(y)
 
-    decoded = masking_layer2(cropped)
+        if conv_arch == '7_conv_layers':
+            cropped = layers.Cropping2D(cropping=((1,2),(3,4)))(y)
 
-    decoder = Model(encoded, decoded, name="decoder")
-    decoder.summary(60)
+        elif conv_arch == '5_conv_layers':
+            cropped = layers.Cropping2D(cropping=((2,1),(2,1)))(y)
+        else:
+            raise Exception(f'invalid conv_arch {conv_arch}')
 
-    autoencoder = Model(state_input, decoded, name="autoencoder")
-    autoencoder.summary(60)
+        decoded = masking_layer(cropped)
 
-    loss = keras.losses.MeanSquaredError(
-        reduction="sum_over_batch_size",
-        name="mean_squared_error"
-    )
+        decoder = Model(encoded, decoded, name="decoder")
+        if verbosity > 10:
+            decoder.summary(60)
 
-    optim = keras.optimizers.Adam(learning_rate=learning_rate)
-    autoencoder.compile(optimizer=optim,
-                        loss=loss)
+        autoencoder = Model(state_input, decoded, name="autoencoder")
+        if verbosity > 5:
+            autoencoder.summary(60)
 
-    return encoder, decoder, autoencoder
+        loss = keras.losses.MeanSquaredError(
+            reduction="sum_over_batch_size",
+            name="mean_squared_error"
+        )
+
+        if optimizer == 'adam':
+            optim = keras.optimizers.Adam(learning_rate=learning_rate)
+        elif optimizer == 'sgd':
+            optim = keras.optimizers.SGD(learning_rate=learning_rate)
+
+        autoencoder.compile(optimizer=optim,
+                            loss=loss)
+
+        # write to log
+        self.log(locals(), 'a')
+        self.log_model(autoencoder, 'a')
+
+        return autoencoder, encoder, decoder
+
+    # build model for hyperparameter tuning
+    def build(self, hp):
+        # activation=hp.Choice("activation", ["relu", "tanh"])
+        optimizer=hp.Choice("optimizer", ["sgd", "adam"])
+        learning_rate = hp.Float("learning_rate",
+                                 min_value=1e-4,
+                                 max_value=5e-2,
+                                 sampling="log")
+        hypermodel, _, _ = self.build_model(learning_rate=learning_rate,
+                                            optimizer=optimizer)
+        return hypermodel
+
+    def fit(self, hp, model, *args, **kwargs):
+        batch_sizes = hp.Int("batch_size",
+                             min_value=2,
+                             max_value=32)
+        return model.fit(*args, batch_size=batch_sizes,
+                         **kwargs)
+
+
+    def log(self, msg, mode='a'):
+        original = sys.stdout
+        with open(self.log_file, mode) as f:
+            sys.stdout = f
+            print(msg)
+            sys.stdout = original
+
+    def log_model(self, model, mode='a'):
+        original = sys.stdout
+        with open(self.log_file, mode) as f:
+            sys.stdout = f
+            print(model.summary())
+            sys.stdout = original
--- autoencoder_original.py
+++ autoencoder_original.py
@@ -15,39 +15,46 @@ from sklearn.preprocessing import MinMaxScaler
 import torch
 import torch.multiprocessing
 import keras
+import keras_tuner
 from keras import layers
 from keras import ops
 from keras.models import Model
 
 import data_manager as dm
 reload(dm)
-import ae_model as ae
-reload(ae)
+import ae_model
+reload(ae_model)
+from ae_model import AutoEncoder
+
 import plot_utils
 reload(plot_utils)
 from plot_utils import PlotMachine
 
+# setup config
 new_experiment=True
+training_mode='tuning'
+do_prediction = True
 if new_experiment:
     create_model_from_scratch=True
     experiment_id = datetime.now().strftime('%Y%m%d_%H%M%S')
-    add_id = '_uv_7_layers'
-    
-    experiment_id = 'testing'
-    add_id = ''
+    add_id = '_tuning'
+
+    # experiment_id = 'tuning'
+    # add_id = ''
 else:
     create_model_from_scratch=False
     add_id = ''
     experiment_id = '20240716_174451_uv_7_layers'
 
 models_dir = f'experiments/{experiment_id}{add_id}/models'
+tuning_dir = f'experiments/{experiment_id}{add_id}/tuning'
 results_dir = f'experiments/{experiment_id}{add_id}/results'
 movie_dir = f'experiments/{experiment_id}{add_id}/movies'
 checkpoints_dir = f'experiments/{experiment_id}{add_id}/checkpoints'
-
 log_file = f'{models_dir}/log.txt'
 
 os.system(f'mkdir -p {models_dir}')
+os.system(f'mkdir -p {tuning_dir}')
 os.system(f'mkdir -p {movie_dir}')
 os.system(f'mkdir -p {results_dir}')
 os.system(f'mkdir -p {checkpoints_dir}')
@@ -98,9 +105,11 @@ model_path_autoencoder = f'{models_dir}/autoencoder_res.keras'
 model_path_encoder = f'{models_dir}/encoder_res.keras'
 model_path_decoder = f'{models_dir}/decoder_res.keras'
 
-
 if create_model_from_scratch:
-    encoder, decoder, autoencoder = ae.build_model(mask=mask)
+    ae = AutoEncoder(test_vec=train_data[0,:,:,:],
+                     mask=mask, log_file=log_file)
+    autoencoder, encoder, decoder = ae.build_model(use_dropout=False)
+
 elif isinstance(model_path_encoder, str):
     encoder = keras.models.load_model(model_path_encoder)
     decoder = keras.models.load_model(model_path_decoder)
@@ -112,8 +121,8 @@ print('--------------------------------------')
 print(f'experiment: {experiment_id}{add_id}')
 print('--------------------------------------')
 
-train_model=True
-if train_model:
+
+if training_mode == 'normal':
     checkpoint_filepath = f'{checkpoints_dir}/checkpoint.model.keras'
 
     mdl_callback = keras.callbacks.ModelCheckpoint(
@@ -138,14 +147,14 @@ if train_model:
     batch_size = 50
     shuffle = True
     tic = time.time()
-    hist = autoencoder.fit(x=train_data,
-                           y=train_data,
-                           epochs=epochs,
-                           batch_size=batch_size,
-                           shuffle=shuffle,
-                           validation_data=(test_data, test_data),
-                           callbacks=[mdl_callback, tb_callback]
-                           )
+    autoencoder.fit(x=train_data,
+                    y=train_data,
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    shuffle=shuffle,
+                    validation_data=(test_data, test_data),
+                    callbacks=[mdl_callback, tb_callback]
+                    )
     toc = time.time()
     print(f'total training time: {(toc-tic)/60}m')
 
@@ -154,93 +163,120 @@ if train_model:
     encoder.save(model_path_encoder)
     decoder.save(model_path_decoder)
 
-print('create predictions')
-predictions = autoencoder.predict(test_data)
-encoded_xr_HR_true = encoder.predict(test_data)
-encoded_xr_HR_pred = encoder.predict(predictions)
-encoded_xr_LR_true = encoder.predict(test_LR)
+elif training_mode == 'tuning':
+    do_prediction = False
+    ae_tuning = AutoEncoder(test_vec=train_data[0,:,:,:],
+                            mask=mask, log_file=log_file)
+    epochs = 2
+    batch_size = 50
+    shuffle = True
 
-# write log
-original = sys.stdout
-with open(log_file, 'w') as f:
-    sys.stdout = f
-    print(autoencoder.summary(100))
-    sys.stdout = original
+    tuner = keras_tuner.BayesianOptimization(
+        hypermodel=AutoEncoder(test_vec=train_data[0,:,:,:],
+                               mask=mask, log_file=log_file),
+        objective="val_loss",
+        max_trials=100,
+        # executions_per_trial=2,
+        # max_epochs=2,
+        overwrite=True,
+        directory=tuning_dir,
+        project_name="ae_tuning",
+    )
 
-# Create dictionary for output visualization
-xr_HR_true_fun = lambda i : scaler_HR.inverse_transform(test_data[i,:,:,:]\
-                                                        .reshape(1,-1))\
-                                     .reshape(Nlat, Nlon, num_channels)
+    tuner.search_space_summary()
 
-# total kinetic energy
-Kt_HR_true_fun = lambda i : np.sqrt(np.square(xr_HR_true_fun(i)).sum(axis=2))
+    hist = tuner.search(train_data,
+                        train_data,
+                        epochs=epochs,
+                        #batch_size=batch_size,
+                        shuffle=shuffle,
+                        validation_data=(test_data, test_data))
 
-xr_HR_pred_fun = lambda i : scaler_HR.inverse_transform(predictions[i,:,:,:]\
-                                                        .reshape(1,-1))\
-                                     .reshape(Nlat, Nlon, num_channels)
+    tuner.results_summary()
+    breakpoint()
+else:
+    print('-- Skipping training --')
+    pass
 
-Kt_HR_pred_fun = lambda i : np.sqrt(np.square(xr_HR_pred_fun(i)).sum(axis=2))
+if do_prediction:
 
-xr_HR_diff_fun = lambda i : xr_HR_true_fun(i) - xr_HR_pred_fun(i)
+    print('create predictions')
+    predictions = autoencoder.predict(test_data)
+    encoded_xr_HR_true = encoder.predict(test_data)
+    encoded_xr_HR_pred = encoder.predict(predictions)
+    encoded_xr_LR_true = encoder.predict(test_LR)
 
-Kt_HR_diff_fun = lambda i : Kt_HR_true_fun(i) - Kt_HR_pred_fun(i)
+    # Create dictionary for output visualization
+    xr_HR_true_fun = lambda i : scaler_HR.inverse_transform(test_data[i,:,:,:]\
+                                                            .reshape(1,-1))\
+                                         .reshape(Nlat, Nlon, num_channels)
 
-Rs_true_fun = lambda i : test_data[i,:,:,0] - test_LR[i,:,:,0]
-Rs_pred_fun = lambda i : predictions[i,:,:,0] - test_LR[i,:,:,0]
-Rs_diff_fun = lambda i : Rs_true_fun(i) - Rs_pred_fun(i)
+    # total kinetic energy
+    Kt_HR_true_fun = lambda i : np.sqrt(np.square(xr_HR_true_fun(i)).sum(axis=2))
 
-enc_xr_HR_k_fun = lambda i,k : (encoded_xr_HR_true[i,:,:,k])
-enc_vmax = lambda i : np.max(encoded_xr_HR_true[:,:,:,i])
-enc_vmin = lambda i : np.min(encoded_xr_HR_true[:,:,:,i])
+    xr_HR_pred_fun = lambda i : scaler_HR.inverse_transform(predictions[i,:,:,:]\
+                                                            .reshape(1,-1))\
+                                         .reshape(Nlat, Nlon, num_channels)
 
-output_dict = {'Kt_HR true' : {'values' : Kt_HR_true_fun,
-                               'vmin' : 0,
-                               'vmax' : .8,
-                               'cmap' : 'viridis'},
-               'Kt_HR pred' : {'values' : Kt_HR_pred_fun,
-                               'vmin' : 0,
-                               'vmax' : .8,
-                               'cmap' : 'viridis'},
-               'Kt_HR diff' : {'values' : Kt_HR_diff_fun,
-                               'vmin' : -0.1,
-                               'vmax' : 0.1,
-                               'cmap' : 'RdBu'},
+    Kt_HR_pred_fun = lambda i : np.sqrt(np.square(xr_HR_pred_fun(i)).sum(axis=2))
 
-               'res true' : {'values' : Rs_true_fun,
-                             'vmin' : -0.1,
-                             'vmax' : 0.1,
-                             'cmap' : 'RdBu'},
-               'res pred' : {'values' : Rs_pred_fun,
-                             'vmin' : -0.1,
-                             'vmax' : 0.1,
-                             'cmap' : 'RdBu'},
-               'res dif' : {'values' : Rs_diff_fun,
-                            'vmin' : -0.05,
-                            'vmax' : 0.05,
-                            'cmap' : 'RdBu'},
+    xr_HR_diff_fun = lambda i : xr_HR_true_fun(i) - xr_HR_pred_fun(i)
 
-               'enc xr_HR ch:0' : {'values' : lambda i : enc_xr_HR_k_fun(i,0),
-                                   'vmin' : enc_vmin(0),
-                                   'vmax' : enc_vmax(0),
+    Kt_HR_diff_fun = lambda i : Kt_HR_true_fun(i) - Kt_HR_pred_fun(i)
+
+    Rs_true_fun = lambda i : test_data[i,:,:,0] - test_LR[i,:,:,0]
+    Rs_pred_fun = lambda i : predictions[i,:,:,0] - test_LR[i,:,:,0]
+    Rs_diff_fun = lambda i : Rs_true_fun(i) - Rs_pred_fun(i)
+
+    enc_xr_HR_k_fun = lambda i,k : (encoded_xr_HR_true[i,:,:,k])
+    enc_vmax = lambda i : np.max(encoded_xr_HR_true[:,:,:,i])
+    enc_vmin = lambda i : np.min(encoded_xr_HR_true[:,:,:,i])
+
+    output_dict = {'Kt_HR true' : {'values' : Kt_HR_true_fun,
+                                   'vmin' : 0,
+                                   'vmax' : .8,
                                    'cmap' : 'viridis'},
-               'enc xr_HR ch:1' : {'values' : lambda i : enc_xr_HR_k_fun(i,1),
-                                   'vmin' : enc_vmin(1),
-                                   'vmax' : enc_vmax(1),
+                   'Kt_HR pred' : {'values' : Kt_HR_pred_fun,
+                                   'vmin' : 0,
+                                   'vmax' : .8,
                                    'cmap' : 'viridis'},
-               'enc xr_HR ch:2' : {'values' : lambda i : enc_xr_HR_k_fun(i,2),
-                                   'vmin' : enc_vmin(2),
-                                   'vmax' : enc_vmax(2),
-                                   'cmap' : 'viridis'},
-               }
+                   'Kt_HR diff' : {'values' : Kt_HR_diff_fun,
+                                   'vmin' : -0.1,
+                                   'vmax' : 0.1,
+                                   'cmap' : 'RdBu'},
 
-reload(plot_utils)
-from plot_utils import PlotMachine
+                   'res true' : {'values' : Rs_true_fun,
+                                 'vmin' : -0.1,
+                                 'vmax' : 0.1,
+                                 'cmap' : 'RdBu'},
+                   'res pred' : {'values' : Rs_pred_fun,
+                                 'vmin' : -0.1,
+                                 'vmax' : 0.1,
+                                 'cmap' : 'RdBu'},
+                   'res dif' : {'values' : Rs_diff_fun,
+                                'vmin' : -0.05,
+                                'vmax' : 0.05,
+                                'cmap' : 'RdBu'},
 
-plotmachine = PlotMachine(output_dict=output_dict,
-                          results_dir=results_dir,
-                          movie_dir=movie_dir,
-                          time_array=test_time)
+                   'enc xr_HR ch:0' : {'values' : lambda i : enc_xr_HR_k_fun(i,0),
+                                       'vmin' : enc_vmin(0),
+                                       'vmax' : enc_vmax(0),
+                                       'cmap' : 'viridis'},
+                   'enc xr_HR ch:1' : {'values' : lambda i : enc_xr_HR_k_fun(i,1),
+                                       'vmin' : enc_vmin(1),
+                                       'vmax' : enc_vmax(1),
+                                       'cmap' : 'viridis'},
+                   'enc xr_HR ch:2' : {'values' : lambda i : enc_xr_HR_k_fun(i,2),
+                                       'vmin' : enc_vmin(2),
+                                       'vmax' : enc_vmax(2),
+                                       'cmap' : 'viridis'},
+                   }
 
-plotmachine.plot_single_frame(100)
-plotmachine.create_movie()
-plotmachine.plot_history(hist)
+    plotmachine = PlotMachine(output_dict=output_dict,
+                              results_dir=results_dir,
+                              movie_dir=movie_dir,
+                              time_array=test_time)
+
+    plotmachine.plot_single_frame(100)
+    plotmachine.create_movie()
+    plotmachine.plot_history(hist)

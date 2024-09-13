6c06e6d63c2d6bf2207dca08636c2874eaac938e
erik mulder <erik.mulder@smhi.se>
2024-09-13 23:41:28 +0200

    ok submit script

---
+++ submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -t 04:00:00
+#SBATCH -J tuning
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --cpus-per-task 4
+#SBATCH --mem 32G
+#SBATCH --qos low
+
+export OMP_NUM_THREADS=4
+export MKL_NUM_THREADS=4
+export OPENBLAS_NUM_THREADS=4
+
+# module load Mambaforge/23.3.1-1-hpc1
+# mamba activate freja_stable
+python autoencoder_original.py

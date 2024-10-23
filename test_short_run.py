4db2fe4e7ee8f5558cac3be12890d379a41f981c
erik mulder <erik.mulder@smhi.se>
2024-10-23 17:18:42 +0200

    not sure why reloading is necessary but ok

--- test_short_run.py
+++ test_short_run.py
@@ -1,20 +1,30 @@
+from importlib import reload
 import autoencoder_original
+reload(autoencoder_original)
 from autoencoder_original import AE_Experiment
 
-exp = AE_Experiment(
-    exp_name='test_short_run',
-    tuning_config='default',
-    detide=False,
-    compute_data=False,
-    coarsening_method='gaussian_filter',
-    truncation=100,
-    sigma=[1,1.5,1.5],
-    feedthrough_type='inactive')
 
-# adjust hyperparameters:
-test_pars = {'history' : 1000,
-             'future' : 100,
-             'epochs' : 1,
-             'batch_size' : 4}
-exp.hyper_params.update(test_pars)
-exp.build_and_run_model()
+def test_short_run():
+
+    exp = AE_Experiment(
+        exp_name='test_short_run',
+        tuning_config='default',
+        detide=False,
+        compute_data=False,
+        coarsening_method='gaussian_filter',
+        sigma=[1,1.5,1.5],
+        feedthrough_type='hybrid')
+
+    # adjust hyperparameters:
+    test_pars = {'history' : 1000,
+                 'future' : 100,
+                 'epochs' : 1,
+                 'batch_size' : 4}
+
+    exp.hyper_params.update(test_pars)
+
+    exp.build_and_run_model()
+
+
+if __name__=="__main__":
+    test_short_run()

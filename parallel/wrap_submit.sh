3b0258ff867fb574ff8584f8edf00e6146c730d2
erik mulder <erik.mulder@smhi.se>
2024-10-25 14:28:29 +0200

    studying what I have

--- parallel/wrap_submit.sh
+++ parallel/wrap_submit.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 function run_sbatch {
-    sbcommand=`sbatch $1`
+    sbcommand=`sbatch $1 $2`
     if [[ "$sbcommand" =~ Submitted\ batch\ job\ ([0-9]+) ]]; then
         jobid=${BASH_REMATCH[1]}
         echo "$jobid"

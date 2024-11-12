59b48b68e7a3830510978c14b5f625ee90a840df
erik mulder <erik.mulder@smhi.se>
2024-11-12 20:28:18 +0100

    quite a good baseline

--- parallel/submit.sh
+++ parallel/submit.sh
@@ -3,17 +3,22 @@
 #SBATCH -t 04:00:00
 #SBATCH -J tuning
 #SBATCH -N 1
-#SBATCH -n 16
-#SBATCH --cpus-per-task 2
+#SBATCH -n 4
+#SBATCH --cpus-per-task 4
 #SBATCH --mem 36G
 #SBATCH --qos low
 
-export OMP_NUM_THREADS=16
-export MKL_NUM_THREADS=16
-export OPENBLAS_NUM_THREADS=16
+export OMP_NUM_THREADS=8
+export MKL_NUM_THREADS=8
+export OPENBLAS_NUM_THREADS=8
 
-# module load Mambaforge/23.3.1-1-hpc1
-# mamba activate freja_stable
+module load buildtool-easybuild/4.9.0-hpc434151c3d \
+       GCCcore/11.3.0 \
+       FFmpeg/4.4.2
+
+module load Mambaforge/23.3.1-1-hpc1
+mamba activate freja_stable
+
 origdir=$PWD
 cd ../
 
--- predict_existing.py
+++ predict_existing.py
@@ -12,18 +12,24 @@ model = {'folder' : 'experiments/gaussian_FT_hybrid_2lrs-default/models/',
 model = {'folder' : 'experiments/gaussian_inactive_FT-default/models/',
          'postfix' : 'trial_1_20241005_204321'}
 
+model = {
+    'folder' : 'experiments/test_RNN-default/models/',
+    'postfix' : 'trial_45_20241101_203528'
+}
+
+
 exp = AE_Experiment(existing_model=model,
-                    exp_name='restart_gaussian_FT_inactive_2lrs',
+                    exp_name='restart_RNN',
                     tuning_config='default',
                     detide=False,
                     compute_data=False,
                     coarsening_method='gaussian_filter',
                     truncation=100,
                     sigma=[1,1.5,1.5],
-                    feedthrough_type='inactive')
+                    feedthrough_type='hybrid')
 
 exp.hyper_params['future']='all'
 
 exp.build_and_run_model(predict_only=True,
                         evaluate=False)
-# exp.create_movie()
+exp.create_movie()

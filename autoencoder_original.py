1cb82e69cb5d2a067aa7f09eb08e3ec95b64bd97
erik mulder <erik.mulder@smhi.se>
2024-11-19 17:11:09 +0100

    factorized configs

--- autoencoder_original.py
+++ autoencoder_original.py
@@ -1,5 +1,7 @@
+import importlib
 from importlib import reload
 
+import rich
 import os
 import sys
 import dill
@@ -100,177 +102,40 @@ class AE_Experiment():
         # -------------------------------------------------------
         self.trial_id = None
 
-        # default hyperparams
-        self.hyper_params = {
-            'history' : 'all',
-            'lookback' : 2,
-            'unroll_dim' : 0,
-            'num_conv_blocks' : 2,
-            'conv_layers_per_block' : 2,
-            'num_feedthrough_layers' : 1,
-            'num_feedthrough_filters' : None,
-            'num_output_layers' : 2,
-            'future' : 400,
-            'noise_stddev' : 0.0,
-            'dropout_rate' : 0.0,
-            'optimizer' : 'adam',
-            'L2_lambda' : 0.0,
-            'kernel_size' : (3,3),
-            'RNN_model' : 'RNN',
-            'RNN_dim' : 32,
-            'epochs' : 4,
-            'batch_size' : 4,
-            'learning_rate' : 0.002,
-            'num_filters' : 32,
-            'num_filters_last' : 8,
-            'downsample_stride' : (2,2),
-        }
-
-        ## maybe read from ini or xml instead?
-        self.tuning_config_dict = {
-            #-------------------------------------------------------
-            'default' : {
-                'epochs' : {
-                    'type' : 'int',
-                    'args' : {'name':'epochs',
-                              'low': 1,
-                              'high':50},
-                    'search_space' : [20] },
-                
-                'learning_rate' : {
-                    'type' : 'float',
-                    'args' : {'name':'learning_rate',
-                              'low':1e-4,
-                              'high':1e-2},
-                    'search_space' : [2e-3] },
-                
-                'num_conv_blocks' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_conv_blocks',
-                              'low'  : 1,
-                              'high' : 6},
-                    'search_space' : [1] },
-                
-                'conv_layers_per_block' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'conv_layers_per_block',
-                              'low'  : 1,
-                              'high' : 6},
-                    'search_space' : [1] },
-                
-                'num_filters' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_filters',
-                              'low'  : 1,
-                              'high' : 200},
-                    'search_space' : [64] },
-                
-                'num_filters_last' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_filters_last',
-                              'low'  : 1,
-                              'high' : 100},
-                    'search_space' : [112] },
-                
-                'num_feedthrough_layers' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_feedthrough_layers',
-                              'low'  : 1,
-                              'high' : 100},
-                    'search_space' : [2] },
-                
-                'num_feedthrough_filters' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_feedthrough_filters',
-                              'low'  : 1,
-                              'high' : 1000},
-                    'search_space' : [112] },
-                
-                'num_output_layers' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'num_output_layers',
-                              'low'  : 1,
-                              'high' : 5},
-                    'search_space' : [2] },
-                
-                'lookback' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'lookback',
-                              'low'  : 0,
-                              'high' : 9},
-                    'search_space' : [2] },
-
-                'unroll_dim' : {
-                    'type' : 'int',
-                    'args' : {'name' : 'unroll_dim',
-                              'low'  : 0,
-                              'high' : 9},
-                    'search_space' : [0] },
-                
-                'batch_size' : {
-                    'type' : 'int',
-                    'args' : {'name':'batch_size',
-                              'low':1,
-                              'high':100},
-                    'search_space' : [4] },
-
-                'RNN_model' : {
-                    'type' : 'categorical',
-                    'args' : {'name':'RNN_model',
-                              'choices' : ['RNN',
-                                           'RNN_res',
-                                           'LSTM',
-                                           'GRU',
-                                           'ConvLSTM']},
-                    'search_space' : ['RNN'] },
-                
-                'RNN_dim' : {
-                    'type' : 'int',
-                    'args' : {'name':'RNN_dim',
-                              'low':1,
-                              'high':10000},
-                    'search_space' : [8] } },
-
-            #-------------------------------------------------------
-            'regularization' : {
-                'L2_lambda' : {
-                    'type' : 'float',
-                    'args' : {'name' : 'L2_lambda',
-                              'low'  : 0,
-                              'high' : 1e2},
-                    'search_space' : [0, 1e-8, 1e-7, 1e-6, 1e-5] } },
-            
-            #-------------------------------------------------------
-            'training_pars' : {
-                'L2_lambda' : {
-                    'type' : 'float',
-                    'args' : {'name' : 'L2_lambda',
-                              'low'  : 0,
-                              'high' : 1e2},
-                    'search_space' : [0, 1e-10, 1e-8, 1e-6] },
-                'learning_rate' : {
-                    'type' : 'float',
-                    'args' : {'name':'learning_rate',
-                              'low':1e-4,
-                              'high':1e-2},
-                    'search_space' : [2e-3, 4e-3, 6e-3, 8e-3, 1e-2] },
-                'batch_size' : {
-                    'type' : 'int',
-                    'args' : {'name':'batch_size',
-                              'low':1,
-                              'high':100},
-                    'search_space' : [1,2,4,8] } },
-            
-            #-------------------------------------------------------
-            'filters' : {}, #TODO
-            'dropout' : {}, #TODO
-            'skip_connections' : {}, # TODO
-            'layers_per_block' : {}, # TODO
-            #-------------------------------------------------------
-        }
-
-
+        self.load_config(config_name='default')
         self.ct=ComputeTool()
+
+
+    def load_config(self, config_name):
+        # Load a config that lives in the <configs> dir: config_file =
+        # <configs>/<config_name>.py. Overwrite class members and
+        # create new ones according what is present in
+        # config_file. Exclude "__" members and functions.
+
+        config_file = f'configs.{config_name}'
+        print(f'Load config: {config_file}')
+
+        try:
+            config = importlib.import_module(config_file)
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(config_file)
+        
+        importlib.reload(config)
+
+        module_vars = vars(config)
+
+        # load variables
+        config_vars = {
+            key: value for key, value in module_vars.items()
+            if not key.startswith("__") and not callable(value)
+        }
+
+        for (key, value) in config_vars.items():
+            setattr(self, key, value)
+
+        rich.print('default hyper params:', self.hyper_params)
+        rich.print('tuning config dict:', self.tuning_config_dict)           
+
 
 
     def run_optuna_study(self):

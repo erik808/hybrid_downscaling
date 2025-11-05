39f2f26ebc620e500f5a7b5cff8523ee20b03913
erik <erik.mulder@smhi.se>
2025-11-05 10:27:23 +0100

    test leaky relu

--- configs/vae_model.py
+++ configs/vae_model.py
@@ -11,8 +11,8 @@ gamma = 1
 filter_mult_start = 4
 filter_mult_rest = 2
 
-# activation type
-activation='elu'
+# activation type ('relu', 'leaky_relu', 'elu')
+activation='leaky_relu'
 
 # dense dimension
 dense_dim = 256

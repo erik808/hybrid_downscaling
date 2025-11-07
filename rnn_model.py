403c5864d640c1bb27f97c5c0c8cd4c04fd7d2b2
erik <erik.mulder@smhi.se>
2025-11-07 11:22:01 +0100

    deeper conv

--- rnn_model.py
+++ rnn_model.py
@@ -220,13 +220,31 @@ class RNNLayer(layers.Layer):
 
         elif self.mode == 'conv3d':
             self.input_transf = Stack()
-            self.model = layers.Conv3D(
-                filters=128,
-                kernel_size=3,
-                strides=1,
-                padding='same',
-                activation='leaky_relu',
-            )
+
+            self.model = keras.Sequential([
+                layers.Conv3D(
+                    filters=256,
+                    kernel_size=3,
+                    strides=1,
+                    padding='same',
+                    activation='leaky_relu',
+                ),
+                layers.Conv3D(
+                    filters=128,
+                    kernel_size=3,
+                    strides=1,
+                    padding='same',
+                    activation='leaky_relu',
+                ),
+                layers.Conv3D(
+                    filters=dims[-1],
+                    kernel_size=3,
+                    strides=1,
+                    padding='same',
+                    activation='leaky_relu',
+                ),
+            ])
+
             self.output_transf = keras.Sequential([
                 layers.MaxPooling3D(
                     pool_size=(lb_dim, 1, 1),

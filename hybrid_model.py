cde8a5dc3a40b42b8b16eb422a2fafd4488102c8
erik <erik.mulder@smhi.se>
2025-11-07 15:19:49 +0100

    began work on hybrid model

---
+++ hybrid_model.py
@@ -0,0 +1,26 @@
+import torch
+import keras
+from keras import ops
+from keras import layers
+
+import numpy as np
+import importlib
+import tools
+import base_model
+import resnet_model
+import predictor_model
+
+importlib.reload(base_model)
+importlib.reload(resnet_model)
+importlib.reload(predictor_model)
+
+
+class Hybrid(base_model.BaseModel):
+    def __init__(
+            self,
+            resnet_model,
+            predictor_model,
+            **kwargs,
+    ):
+        super().__init__(**kwargs)
+        tools.load_config(self, config_name='hybrid_model')

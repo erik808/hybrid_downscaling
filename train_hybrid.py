6ec0e9125740b07f9f0c78ebac99c3a1ed6e986a
erik mulder <erik.mulder@smhi.se>
2025-11-18 09:21:58 +0100

    stop loading

--- train_hybrid.py
+++ train_hybrid.py
@@ -50,7 +50,7 @@ resnet = resnet_model.ResNet(data_gen=dgen_train)
 resnet.build_model("ResNet")
 resnet.summary(expand_nested=True)
 resnet_checkpoint = 'models/resnet/b1f4fh128o2/checkpoint.resnet.keras'
-resnet.load_weights(resnet_checkpoint, skip_mismatch=True)
+# resnet.load_weights(resnet_checkpoint, skip_mismatch=True)
 
 vae = vae_model.VAE(data_gen=dgen_train)
 vae.build_model("betaVAE")

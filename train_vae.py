8ffcdb12df64a30c9bd85e58472b907561e52d97
erik <erik.mulder@smhi.se>
2025-11-26 21:39:14 +0100

    more epochs

--- train_vae.py
+++ train_vae.py
@@ -62,7 +62,7 @@ model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
 # raise Exception('que?')
 hist = vae.fit(
     x=dgen_train,
-    epochs=5,
+    epochs=50,
     validation_data=dgen_test,
     callbacks=[
         analysis_callback,
